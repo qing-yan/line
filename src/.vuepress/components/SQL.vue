324cd5927e97e39d7df25dc847ce5a9208ae101f
qing-yan <190250664@qq.com>
2023-12-20 23:35:56 +0800

    添加CONCAT更新功能

--- src/.vuepress/components/SQL.vue
+++ src/.vuepress/components/SQL.vue
@@ -3,8 +3,9 @@
         <span>组合UPDATE语句，数据格式为Excel格式</span>
         <el-input v-model="tableName" placeholder="表名" style="width: 100%" clearable/>
         <el-input v-model="text" type="textarea" :rows="10" placeholder="内容" style="width: 100%"/>
-        <el-button type="primary" @click="decode()" style="margin-top: 5px; margin-bottom: 5px">解析</el-button>
-        <el-button type="primary" @click="decode2()" style="margin-top: 5px; margin-bottom: 5px">SELECT</el-button>
+        <el-button type="primary" @click="update('clear')" style="margin-top: 5px; margin-bottom: 5px">CLEAR UPDATE</el-button>
+        <el-button type="primary" @click="update('concat')" style="margin-top: 5px; margin-bottom: 5px">CONCAT UPDATE</el-button>
+        <el-button type="primary" @click="select()" style="margin-top: 5px; margin-bottom: 5px">SELECT</el-button>
         <el-card style="white-space: pre-line;">
             {{ result }}
         </el-card>
@@ -24,7 +25,7 @@ export default {
     },
     methods: {
         //解析
-        decode() {
+        update(type) {
             //按行切片
             let rows = this.text.split("\n")
             for (let i = 0; i < rows.length; i++) {
@@ -37,10 +38,19 @@ export default {
                 const e = this.datas[i];
                 var conditionFieldValue = e[0]
                 var fields = `${this.tableName}.${this.datas[0][1]} = '${e[1]}'`
+                if (type == 'clear') {
+                    fields = `${this.tableName}.${this.datas[0][1]} = '${e[1]}'`
+                } else {
+                    fields = `${this.tableName}.${this.datas[0][1]} = CONCAT(${this.tableName}.${this.datas[0][1]}, '${e[1]}')`
+                }
                 //多字段更新
                 if (e.length > 2) {
                     for (let j = 2; j < e.length; j++) {
-                        fields += `,${this.tableName}.${this.datas[0][j]} = '${e[j]}'`
+                        if (type == 'clear') {
+                            fields += `,${this.tableName}.${this.datas[0][j]} = '${e[j]}'`
+                        } else {
+                            fields += `,${this.tableName}.${this.datas[0][j]} = CONCAT(${this.tableName}.${this.datas[0][j]}, '${e[j]}')`
+                        }
                     }
                 }
                 var sql = `UPDATE ${this.tableName} SET ${fields} WHERE ${this.tableName}.${conditionField} = '${conditionFieldValue}';`
@@ -48,7 +58,7 @@ export default {
             }
             this.copy(this.result)
         },
-        decode2() {
+        select() {
             this.result = '\'' + this.text.replaceAll('\n', '\',\'') + '\''
             this.copy(this.result)
         },

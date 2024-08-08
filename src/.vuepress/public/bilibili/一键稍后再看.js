4ccb795c1742b3143ee865fc3ad7f75624a6cdfa
qing-yan <190250664@qq.com>
2024-08-08 14:40:20 +0800

    已修改        src/.vuepress/public/bilibili/一键稍后再看.js 已修改        src/tools/tidcard.md

--- src/.vuepress/public/bilibili/一键稍后再看.js
+++ src/.vuepress/public/bilibili/一键稍后再看.js
@@ -13,6 +13,7 @@
     'use strict';
     //等待页面加载完毕
     window.onload = function() {
+        deleteDT();
         //打印标记
         console.log('B站动态列表稍后再看按钮扩大并自动加入脚本已加载');
         //获取上次添加时间
@@ -22,20 +23,12 @@
             约${((new Date().getTime() - lastAddTime.getTime()) / 1000 / 3600).toFixed(1)}小时前`, '100px')
         }
         var button = addButton('稍后再看', '500px')
-        var num = 0
-            //添加监听功能
+        var num = 0;
+        //添加监听功能
         button.addEventListener('click', function() {
             //获取所有bili-dyn-list__item，即动态列表
             var dynList = document.querySelectorAll('.bili-dyn-list__item');
             dynList.forEach((dyn) => {
-                //隐藏直播动态
-                var dyn_zhi_bo = dyn.querySelector('.bili-dyn-time.fs-small');
-                if (dyn_zhi_bo != null) {
-                    if (dyn_zhi_bo.innerText === '直播了' | dyn_zhi_bo.innerText === '预约的直播') {
-                        dyn.style.display = 'none';
-                        return;
-                    }
-                }
                 //获取bili-dyn-card-video，即视频组件
                 var dynVideo = dyn.querySelector('.bili-dyn-card-video');
                 //如果为空则不是视频动态，不为空才是
@@ -59,6 +52,49 @@
         });
     };
 })();
+
+function deleteDT() {
+    var ddt = addButton('删除冗余动态', '200px');
+    ddt.addEventListener('click', function() {
+        //获取up主黑名单
+        var black_upname = JSON.parse(localStorage.getItem('black_upname'));
+        if (black_upname == null) {
+            black_upname = [];
+            localStorage.setItem('black_upname', JSON.stringify(black_upname));
+        }
+        //获取动态列表
+        var dynList = document.querySelectorAll('.bili-dyn-list__item');
+        for (let index = 0; index < dynList.length; index++) {
+            const element = dynList[index];
+            //删除直播动态
+            var dyn_zhi_bo = element.querySelector('.bili-dyn-time.fs-small');
+            if (dyn_zhi_bo != null) {
+                if (dyn_zhi_bo.innerText === '直播了' | dyn_zhi_bo.innerText === '预约的直播') {
+                    element.remove();
+                    continue;
+                }
+            }
+            //删除黑名单UP视频动态
+            var upname_div = element.querySelector('div.bili-dyn-title');
+            if (upname_div == null) {
+                //懒加载原因可能为空，所以跳过
+                continue;
+            }
+            //获取up主名字
+            var upname = upname_div.innerText;
+            //如果up主在黑名单中
+            if (black_upname.indexOf(upname) != -1) {
+                //如果是视频动态
+                if (element.querySelector('.bili-dyn-item__desc').innerText.indexOf('投稿了视频') != -1) {
+                    //删除动态
+                    element.remove();
+                }
+            }
+        }
+        
+    })
+}
+
 /**
  * 创建按钮
  * @returns 
@@ -86,4 +122,28 @@ function addButton(text, top) {
     //打印标记
     console.log('按钮已创建');
     return button;
+}
+/**
+ */
+function addDiv(text) {
+    //在网页最上层创建按钮
+    var my_div = document.createElement('div');
+    //设置按钮的文本内容
+    my_div.innerText = text;
+    //设置按钮的样式，位于网页左侧居中
+    my_div.style.position = 'fixed';
+    my_div.style.top = '65px';
+    my_div.style.right = '10px';
+    my_div.style.zIndex = '9999';
+    //背景色天蓝色
+    my_div.style.backgroundColor = '#87cefa';
+    //修改border
+    my_div.style.borderRadius = '5px';
+    my_div.style.border = '1px solid #ccc';
+    my_div.style.padding = '10px';
+    my_div.style.fontSize = '16px';
+    my_div.style.cursor = 'pointer';
+    //将按钮添加到网页中
+    document.body.appendChild(my_div);
+    return my_div;
 }

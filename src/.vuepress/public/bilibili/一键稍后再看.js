b61f9ed29fb1e93aef812e21f576a0eee40421a9
qing-yan <190250664@qq.com>
2024-11-26 22:44:20 +0800

    已修改        src/.vuepress/components/myWorld.vue 已修改        src/.vuepress/public/bilibili/一键稍后再看.js

--- src/.vuepress/public/bilibili/一键稍后再看.js
+++ src/.vuepress/public/bilibili/一键稍后再看.js
@@ -16,85 +16,104 @@
     window.onload = function() {
         //打印标记
         console.log('B站动态列表稍后再看按钮扩大并自动加入脚本已加载');
-        //屏蔽部分动态
-        deleteDT();
-        //获取上次添加时间
-        var lastAddTime = new Date(Number(localStorage.getItem('lastAddTime')));
-        if (lastAddTime != null) {
-            addButton(`上次添加时间${lastAddTime.toLocaleString()}
-            约${((new Date().getTime() - lastAddTime.getTime()) / 1000 / 3600).toFixed(1)}小时前`, '100px')
-        }
+        //添加标记动态
+        add_tag();
         var button = addButton('稍后再看', '500px')
-        var num = 0;
         //添加监听功能
-        button.addEventListener('click', function() {
-            //获取所有bili-dyn-list__item，即动态列表
-            var dynList = document.querySelectorAll('.bili-dyn-list__item');
-            dynList.forEach((dyn) => {
-                //获取bili-dyn-card-video，即视频组件
-                var dynVideo = dyn.querySelector('.bili-dyn-card-video');
-                //如果为空则不是视频动态，不为空才是
-                if (dynVideo != null) {
-                    var but = dynVideo.querySelector('.bili-dyn-card-video__mark:not(.active)')
-                    if (but != null) {
-                        if (dyn.querySelector('.bili-dyn-content__orig:not(.reference)')) {
-                            but.click();
-                            //隐藏动态
-                            dyn.style.display = 'none';
-                            num += 1
-                        }
-                    }
-                }
-                // }
-            });
-
-            button.innerText = `稍后再看+${num}`;
-            //本地储存添时间
-            localStorage.setItem('lastAddTime', new Date().getTime());
+        button.addEventListener('click', () => {
+            // 调用删除函数，可能用于清理或重置某些状态
+            delete_func();
+            // 设置一个定时器，等待500毫秒后执行添加函数
+            // 这里可能需要一点时间间隔以确保某些操作完成或状态更新
+            setTimeout(() => {
+                // 调用添加函数，传入button作为参数
+                // 此操作可能是在状态重置或界面更新后，重新启用或显示某个按钮
+                add_func(button);
+            }, 500);
         });
     };
 })();
-
-function deleteDT() {
-    var ddt = addButton('删除冗余动态', '200px');
-    ddt.addEventListener('click', function() {
-        //获取up主黑名单
-        var black_upname = getData();
-        //获取动态列表
-        var dynList = document.querySelectorAll('.bili-dyn-list__item');
-        var count_zb = 0;
-        var count_bu = 0;
-        for (let index = 0; index < dynList.length; index++) {
-            const element = dynList[index];
-            //删除直播动态
-            var dyn_zhi_bo = element.querySelector('.bili-dyn-time.fs-small');
-            if (dyn_zhi_bo != null) {
-                if (dyn_zhi_bo.innerText === '直播了' | dyn_zhi_bo.innerText === '预约的直播') {
-                    count_zb += 1;
-                    element.remove();
-                    continue;
-                }
-            }
-            //删除黑名单UP视频动态
-            var upname_div = element.querySelector('div.bili-dyn-title');
-            if (upname_div == null) {
-                //懒加载原因可能为空，所以跳过
-                continue;
-            }
-            //获取up主名字
-            var upname = upname_div.innerText;
-            //如果up主在黑名单中
-            if (black_upname.indexOf(upname) != -1) {
-                //如果是视频动态
-                if (element.querySelector('.bili-dyn-item__desc').innerText.indexOf('投稿了视频') != -1) {
-                    count_bu += 1;
-                    //删除动态
-                    element.remove();
+function add_func(button) {
+    var num = 0;
+    //获取所有bili-dyn-list__item，即动态列表
+    var dynList = document.querySelectorAll('.bili-dyn-list__item');
+    dynList.forEach((dyn) => {
+        //获取bili-dyn-card-video，即视频组件
+        var dynVideo = dyn.querySelector('.bili-dyn-card-video');
+        //如果为空则不是视频动态，不为空才是
+        if (dynVideo != null) {
+            var but = dynVideo.querySelector('.bili-dyn-card-video__mark:not(.active)')
+            if (but != null) {
+                if (dyn.querySelector('.bili-dyn-content__orig:not(.reference)')) {
+                    but.click();
+                    //隐藏动态
+                    dyn.style.display = 'none';
+                    num += 1
                 }
             }
         }
-        console.log(`屏蔽直播动态${count_zb}条，屏蔽黑名单UP主视频动态${count_bu}条`);
-    })
+    });
+    button.innerText = `稍后再看+${num}`;
+};
+
+function delete_func() {
+    //获取up主黑名单
+    var black_upname = getData();
+    //获取动态列表
+    var dynList = document.querySelectorAll('.bili-dyn-list__item');
+    var count_zb = 0;
+    var count_bu = 0;
+    for (let index = 0; index < dynList.length; index++) {
+        const element = dynList[index];
+        //删除直播动态
+        var dyn_zhi_bo = element.querySelector('.bili-dyn-time.fs-small');
+        if (dyn_zhi_bo != null) {
+            if (dyn_zhi_bo.innerText === '直播了' | dyn_zhi_bo.innerText === '预约的直播') {
+                count_zb += 1;
+                element.remove();
+                continue;
+            }
+        }
+        //删除黑名单UP视频动态
+        var upname_div = element.querySelector('div.bili-dyn-title');
+        if (upname_div == null) {
+            //懒加载原因可能为空，所以跳过
+            continue;
+        }
+        //获取up主名字
+        var upname = upname_div.innerText;
+        //如果up主在黑名单中
+        if (black_upname.indexOf(upname) != -1) {
+            //如果是视频动态
+            if (element.querySelector('.bili-dyn-item__desc').innerText.indexOf('投稿了视频') != -1) {
+                count_bu += 1;
+                //删除动态
+                element.remove();
+            }
+        }
+    }
+    console.log(`屏蔽直播动态${count_zb}条，屏蔽黑名单UP主视频动态${count_bu}条`);
+};
+function add_tag() {
+    var ddt = addButton('添加标记动态', '200px');
+    ddt.addEventListener('click', () => {
+        console.log('添加标记动态');
+        const inputElement = document.querySelector('.bili-rich-textarea__inner')
+        inputElement.focus();
+        // 检查事件冒泡和捕获
+        inputElement.addEventListener('input', (event) => {
+            console.log('Input event triggered:', event);
+        });
+        setTimeout(() => {
+            const characters = '========================================================'
+            inputElement.innerText = characters;
+            inputElement.dispatchEvent(new Event('input', { bubbles: true }));
+            console.log(inputElement.innerText + '222')
+        }, 500);
+        setTimeout(() => {
+            document.querySelector('.bili-dyn-publishing__action.launcher').click();
+        }, 500);
+    });
 }
 
 /**

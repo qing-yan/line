e345d2a28b1dbbe62c2ab9b198b5b9407fb8be52
qing-yan <190250664@qq.com>
2026-02-09 18:11:25 +0800

    已修改        script/bilibili/t/blackUpList.js 已修改        script/bilibili/稍后再看列表页面/稍后再看数据统计v4.js

--- script/bilibili/稍后再看列表页面/稍后再看数据统计v4.js
+++ script/bilibili/稍后再看列表页面/稍后再看数据统计v4.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.bilibili.com/watchlater/*
-// @require      file://D:/workspace/work/tampermonkey/shzksjtjv4.js
+// @require      file:///D:/workspace/work/tampermonkey/shzksjtjv4.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
 // ==/UserScript==
